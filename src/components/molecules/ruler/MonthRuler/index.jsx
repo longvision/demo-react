cc1993b81bf86eb954f6dcaddd01bc786fb049c0
Ricardo <r.n.hori@gmail.com>
2021-07-19 17:49:15 -0300

    [Style] Rulers styles fixed.

--- src/components/molecules/ruler/MonthRuler/index.jsx
+++ src/components/molecules/ruler/MonthRuler/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Box, Grid, Typography } from '@material-ui/core';
+import { Box, Button, Grid, Typography } from '@material-ui/core';
 import Ruler from '../../../atoms/ruler/Ruler';
 import { setMonthName } from '../../../../utils/dates.js';
 
@@ -10,9 +10,15 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 15,
   },
 
-  button: { border: 'none' },
+  button: {
+    width: '90%',
+    borderRadius: 15,
+    borderColor: '#BFBFBF',
+    '&:hover': { backgroundColor: 'transparent' },
+  },
 }));
 const MonthRuler = ({
   disabled, setMonth, month, handleClick,
@@ -20,31 +26,44 @@ const MonthRuler = ({
   const classes = useStyles();
   return (
     <Grid container className={classes.container}>
-      <Grid item md={1} lg={1} sm={1} xl={1} xs={2} align="center">
-        <Typography variant="h4" color={disabled ? 'textSecondary' : 'primary'}>
-          <strong>JAN</strong>
-        </Typography>
-      </Grid>
-      <Grid item md={10} lg={10} sm={10} xl={10} xs={8} align="center">
-        <Ruler
-          title=""
-          defaultValue={0}
-          step={1}
-          min={0}
-          max={11}
-          labelFunction={setMonthName}
-          disabled={disabled}
-          handleChange={setMonth}
-          value={month}
-          marks
-          handleToggle={handleClick}
-        />
-      </Grid>
-      <Grid item md={1} lg={1} sm={1} xl={1} xs={2} align="center">
-        <Typography variant="h4" color={disabled ? 'textSecondary' : 'primary'}>
-          <strong>DEZ</strong>
-        </Typography>
-      </Grid>
+      <Button
+        type="button"
+        className={classes.button}
+        onClick={handleClick}
+        variant="outlined"
+      >
+        <Grid item md={1} lg={1} sm={1} xl={1} xs={2} align="center">
+          <Typography
+            variant="h3"
+            color={disabled ? 'textSecondary' : 'primary'}
+          >
+            <strong>JAN</strong>
+          </Typography>
+        </Grid>
+        <Grid item md={10} lg={10} sm={10} xl={10} xs={8} align="center">
+          <Ruler
+            title=""
+            defaultValue={0}
+            step={1}
+            min={0}
+            max={11}
+            labelFunction={setMonthName}
+            disabled={disabled}
+            handleChange={setMonth}
+            value={month}
+            marks
+            handleToggle={handleClick}
+          />
+        </Grid>
+        <Grid item md={1} lg={1} sm={1} xl={1} xs={2} align="center">
+          <Typography
+            variant="h3"
+            color={disabled ? 'textSecondary' : 'primary'}
+          >
+            <strong>DEZ</strong>
+          </Typography>
+        </Grid>
+      </Button>
     </Grid>
   );
 };
